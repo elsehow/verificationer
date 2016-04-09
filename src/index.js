4324a3698a38750cc784732e28b028363b232fb4
elsehow <yes@cosmopol.is>
2016-04-08 23:47:50 -0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-module.exports = () => {
-  return 'what it isssss'
+var halite = require('halite')
+
+function sign (obj, kp) {
+  var sk     = halite.sk(kp)
+  var str    = JSON.stringify(obj)
+  var signed = halite.sign(str, sk)
+  return signed
+}
+
+function verify (arr, kp) {
+  var pk  = halite.pk(kp)
+  var ver = halite.verify(arr, pk)
+  var obj = null
+  try {
+    obj = JSON.parse(
+      ver.replace(/\\'/g, "'")
+    )
+  } catch (e) {
+    obj = {}
+  }
+  return obj
+}
+
+// takes hyperlog `hl`
+// halite signKeypair `kp`
+// and state callback (state) => {} cb
+module.exports = (hl, kp, cb) => {
+  return {
+
+    _sign: sign,
+    _verify: verify,
+
+    left: () => {
+      
+    },
+
+    right: () => {
+      
+    },
+
+    up: () => {
+      
+    },
+
+    down: () => {
+      
+    },
+  }
 }
--- test/index.js
+++ test/index.js
@@ -1,3 +1,38 @@
-var mod = require('..')
+var test = require('tape')
+var World = require('..')
 
-console.log(mod(), 'yes')
+var hyperlog = require('hyperlog')
+var halite = require('halite')
+var memdb = require('memdb')
+
+var log = hyperlog(memdb(), {
+  valueEncoding: 'json'
+})
+var sign_kp = halite.signKeypair()
+
+var player = World(log, sign_kp, state => {
+  console.log('world state', state)
+})
+
+
+test('we can sign and verify some object', t => {
+  var payload = { move: 'left' }
+  var sig = player._sign(payload, sign_kp)
+  var ver = player._verify(sig, sign_kp)
+  t.deepEqual(ver, payload)
+  t.end()
+})
+
+
+player.left()
+
+player.right()
+
+player.up()
+
+player.up()
+
+
+
+
+// need a keypair
